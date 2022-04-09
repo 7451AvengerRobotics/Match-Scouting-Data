dcf4bca324e4356f0273df65aba8087f5aa6e4c8
Aniket Chadalavada <61917048+ThatCrispyToast@users.noreply.github.com>
2022-04-09 08:24:29 -0400

    Script Cleanup

--- data-normalizer.py
+++ data-normalizer.py
@@ -1,54 +1,67 @@
 #!/usr/bin/env python3
 """
-This is a script to parse a CSV file from our team's AirTable database into easily analyzable data for Google Data Studio.
-
-Author: Karthik Hari
+A script to parse a CSV file from our team's AirTable database into easily analyzable data for Google Data Studio.
 """
+
+__author__ = "Karthik Hari"
+__version__ = "0.1.0"
+__license__ = "CC0-1.0"
 
 import sys
 
-file = sys.argv[1]
+def main(file):
+    with open(file, 'r') as f:
+        data = f.read()
 
-with open(file, 'r') as f:
-    data = f.read()
+        # Replace human-readable input w/ numbers
+        numbered = data.replace(',,', ',0,') \
+            .replace('checked', '1') \
+            .replace('None', '0') \
+            .replace('1. Low Rung', '1') \
+            .replace('2. Mid Rung', '2') \
+            .replace('3. High Rung', '3') \
+            .replace('4. Traversal Rung', '4') \
+            .split('\n')
 
-    # replace all with numbers
-    numbered = data.replace(',,', ',0,') \
-        .replace('checked', '1') \
-        .replace('None', '0') \
-        .replace('1. Low Rung', '1') \
-        .replace('2. Mid Rung', '2') \
-        .replace('3. High Rung', '3') \
-        .replace('4. Traversal Rung', '4') \
-        .split('\n')
+        out = ''
 
-    out = ''
+        print(f'Parsing {len(numbered)} Lines...')
 
-    print(f'parsing {len(numbered)} lines')
+        for line in numbered:
+            columns = line.split(',')
+            if len(columns) == 1:
+                continue  # Skip empty entries
 
-    for line in numbered:
-        columns = line.split(',')
-        if len(columns) == 1:
-            continue  # skip empty entries
+            # Remove all notes since commas are not useful for broad analysis
+            while len(columns) > 8:
+                columns.pop()
 
-        # remove all notes since commas are annoying and not useful for broad analysis
-        while len(columns) > 8:
-            columns.pop()
+            # Check if team number and match are swapped
+            if (columns[0].isdecimal() and (int(columns[0]) < 100 or int(columns[1]) > 500)):
+                t = columns[0]
+                columns[0] = columns[1]
+                columns[1] = t
 
-        # check if the team number and match are swapped
-        if (columns[0].isdecimal() and (int(columns[0]) < 100 or int(columns[1]) > 500)):
-            t = columns[0]
-            columns[0] = columns[1]
-            columns[1] = t
+            # Add zeroes if last column is blank
+            if columns[7] == '':
+                columns[7] = 0
 
-        # add in zeroes if the last column is blank
-        if columns[7] == '':
-            columns[7] = 0
+            # Put csv back together
+            out += ','.join(columns) + '\n'
 
-        # put the csv back together
-        out += ','.join(columns) + '\n'
+    # Write data as bytes
+    with open(file, 'wb') as f:
+        f.write(out.encode('utf-8'))
+        print('Done!')
 
-# write data
-with open(file, 'wb') as f:
-    f.write(out.encode('utf-8'))
-    print('done!')
+
+if __name__ == "__main__":
+    # Take args
+    try:
+        file = sys.argv[1]
+    except IndexError:
+        # Except on no args taken; inform user of correct usage
+        print('Usage: python3 data-normalizer.py <file>')
+        sys.exit(1)
+
+    main(file)
